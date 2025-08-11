1f43b8aba2a442001d291ce4f2633255adce61ef
Awais Ishaq <awaisishaq003@gmail.com>
2025-08-11 06:17:43 +0100

    updated

--- Addtocart.js
+++ Addtocart.js
@@ -3,30 +3,31 @@ import {
   getFirestore,
   collection,
   getDocs,
-  doc, deleteDoc
+  doc,
+  deleteDoc,
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 // Firebase Config
- const firebaseConfig = {
-    apiKey: "AIzaSyBokbTtDucp5tO_5jjTaSNIi-4zk-OqrWg",
-    authDomain: "authentication-dashboard-e2039.firebaseapp.com",
-    projectId: "authentication-dashboard-e2039",
-    storageBucket: "authentication-dashboard-e2039.firebasestorage.app",
-    messagingSenderId: "1000959311145",
-    appId: "1:1000959311145:web:059f9b0de50a095a45cb38",
-    measurementId: "G-2CX47QTV51"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBokbTtDucp5tO_5jjTaSNIi-4zk-OqrWg",
+  authDomain: "authentication-dashboard-e2039.firebaseapp.com",
+  projectId: "authentication-dashboard-e2039",
+  storageBucket: "authentication-dashboard-e2039.firebasestorage.app",
+  messagingSenderId: "1000959311145",
+  appId: "1:1000959311145:web:059f9b0de50a095a45cb38",
+  measurementId: "G-2CX47QTV51",
+};
 let goback = document.getElementById("goback");
 goback.addEventListener("click", () => {
-    window.location.href = "userdashboard.html";
-    });
- const button = document.querySelector('button');
-  button.addEventListener('mouseenter', function() {
-    this.querySelector('span').style.left = '100%';
-  });
-  button.addEventListener('mouseleave', function() {
-    this.querySelector('span').style.left = '-100%';
-  });
+  window.location.href = "userdashboard.html";
+});
+const button = document.querySelector("button");
+button.addEventListener("mouseenter", function () {
+  this.querySelector("span").style.left = "100%";
+});
+button.addEventListener("mouseleave", function () {
+  this.querySelector("span").style.left = "-100%";
+});
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 let cartItem = document.getElementById("loadcart");
@@ -46,7 +47,9 @@ async function showdata() {
                   <p>${data.description}</p>
                   <div class="price">$${data.price}</div>
                   <div class="cart-actions">
-                    <button class="remove-btn" onclick="removeItem('${doc.id}')">Remove</button>
+                    <button class="remove-btn" onclick="removeItem('${
+                      doc.id
+                    }')">Remove</button>
                     
                   </div>
                 </div>
@@ -57,13 +60,11 @@ async function showdata() {
 
 showdata();
 
-
-
 async function removeItem(id) {
-    cartItem.innerHTML = "";
+  cartItem.innerHTML = "";
 
-    await deleteDoc(doc(db, "addtocart", id));
-    showdata()
+  await deleteDoc(doc(db, "addtocart", id));
+  showdata();
 }
 
 window.removeItem = removeItem; // Expose function globally
